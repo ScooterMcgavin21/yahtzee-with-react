20775333695564d7636438e9cc7933f74f41483c
Scooter <scott.skrobel@gmail.com>
2021-05-14 14:11:41 -0600

    added awsomeFont and replaced dice text with icons

--- src/Die.js
+++ src/Die.js
@@ -9,6 +9,9 @@ import "./Die.css";
 // finally using handleClick as callback for onClick.
 // this.props.handleClick comes from > Dice component > this.props.handleClick comes from Game Compoent as a refrence from toggleLock
 class Die extends Component {
+  static defaultProps = {
+    diceWord: ['one', 'two', 'three', 'four', 'five', 'six']
+  };
   constructor(props) {
     super(props);
 
@@ -20,14 +23,16 @@ class Die extends Component {
     this.props.handleClick(this.props.idx); // toggleLocked
   }
   render() {
+    const { diceWord, locked, val, disabled } = this.props;
+    let classes = `Die fas fa-dice-${diceWord[val - 1]} fa-5x `;
+    if (locked) classes += 'Die-locked';
     return (
-      <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
+      <i
+        className={classes}
         onClick={this.handleClick}
+        disabled={disabled}
       >
-        {this.props.val}
-      </button>
+      </i>
     );
   }
 }
